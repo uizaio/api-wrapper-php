952e125d5c19ec5a50dee815c75dcca5ce35f42b
deathemperor <deathemperor@gmail.com>
2019-06-11 17:55:05 +0700

    update test

--- test/index.php
+++ test/index.php
@@ -1,18 +1,36 @@
 <?php
 
-require __DIR__."/../vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
 
-Uiza\Base::setAppId('your-app-id');
-Uiza\Base::setAuthorization('your-authorization');
+Uiza\Base::setAuthorization('your-authorization-key');
 
 try {
+    $response = Uiza\Live::getRegions();
+
+    print_r($response->SINGAPORE);
+
     $params = [
-        'name' => 'Name entity',
-        'url' => 'http://google.com',
-        'inputType' => 'http',
+        "name" => "test event",
+        "mode" => "push",
+        "encode" => 1,
+        "dvr" => 1,
+        "description" => "This is for test event",
+        "poster" => "//image1.jpeg",
+        "thumbnail" => "//image1.jpeg",
+        "linkStream" => [
+            "https://playlist.m3u8"
+        ],
+        "resourceMode" => "single",
+        "region" => $response->VIETNAM
     ];
 
-    var_dump(Uiza\Entity::create($params));
+    try {
+        $live = Uiza\Live::create($params);
+
+        print_r($live);
+    } catch(\Uiza\Exception\ErrorResponse $e) {
+        print($e);
+    }
 } catch (\Uiza\Exception\ErrorResponse $e) {
     print($e);
 }
